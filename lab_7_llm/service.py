ddcf8dd7d280779a043ce9c045a013fd864169d9
Alexander Demidovskij <monadv@yandex.ru>
2025-01-11 13:36:30 +0300

    sync

---
+++ lab_7_llm/service.py
@@ -0,0 +1,19 @@
+"""
+Web service for model inference.
+"""
+# pylint: disable=too-few-public-methods, undefined-variable, unused-import, assignment-from-no-return, duplicate-code
+from pathlib import Path
+
+
+def init_application() -> tuple[FastAPI, LLMPipeline]:
+    """
+    Initialize core application.
+
+    Run: uvicorn reference_service.server:app --reload
+
+    Returns:
+        tuple[fastapi.FastAPI, LLMPipeline]: instance of server and pipeline
+    """
+
+
+app, pipeline = (None, None)
