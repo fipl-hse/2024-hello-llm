bcb937a56270bcea465164c580e9d06e87e17426
cybern00dle <koshech_noskich@mail.ru>
2025-01-15 12:00:47 +0300

    change for a PR

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -35,6 +35,7 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         Returns:
             dict: Dataset key properties
         """
+        pass
 
     @report_time
     def transform(self) -> None:
