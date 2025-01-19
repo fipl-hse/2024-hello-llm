3dcea054c1467bedf5b3eaeb1c05849ecb9d15b6
solonar <zakker090@gmail.com>
2025-01-20 00:44:04 +0300

    all imports

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -3,13 +3,24 @@ Starter for demonstration of laboratory work.
 """
 # pylint: disable= too-many-locals, undefined-variable, unused-import
 from pathlib import Path
+
+from config.lab_settings import LabSettings
+from config.constants import PROJECT_ROOT
+
 from core_utils.llm.time_decorator import report_time
+from lab_7_llm.main import RawDataImporter, RawDataPreprocessor
 
 @report_time
 def main() -> None:
     """
     Run the translation pipeline.
     """
+    settings = LabSettings(PROJECT_ROOT / "lab_7_llm" / "settings.json")
+
+    importer = RawDataImporter(settings.parameters.dataset)
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+
+
     result = None
     assert result is not None, "Demo does not work correctly"
 
