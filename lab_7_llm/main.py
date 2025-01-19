e953dc7f335e701fbaff8c135946f4d9d7d53324
danind <evolved.dinosaur@gmail.com>
2025-01-19 18:41:13 +0300

    fixing other thing bc it actually the one that wasnt working

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -57,7 +57,7 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         analysis = {
             'dataset_number_of_samples': len(self._raw_data),
             'dataset_columns': len(self._raw_data.columns),
-            'dataset_duplicates': self._raw_data.duplicated(),
+            'dataset_duplicates': self._raw_data.duplicated().sum(),
             'dataset_empty_rows': self._raw_data.isnull().any(axis=1).sum(),
             'dataset_sample_min_len': lens.min(),
             'dataset_sample_max_len': lens.max()
