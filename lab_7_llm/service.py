48cdb6bbc929ccd4edacfa06aa446baa619dd7c5
danind <evolved.dinosaur@gmail.com>
2025-01-19 16:19:47 +0300

    added requirements + imports for service

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -2,8 +2,10 @@
 Web service for model inference.
 """
 # pylint: disable=too-few-public-methods, undefined-variable, unused-import, assignment-from-no-return, duplicate-code
+from fastapi import FastAPI
 from pathlib import Path
 
+from lab_7_llm.main import LLMPipeline
 
 def init_application() -> tuple[FastAPI, LLMPipeline]:
     """
