9ddeb4313b8ea334da3df0dd5f3f514b04d65b93
Christina Nigamedzyanova <kristinanigamedzanova@gmail.com>
2025-01-26 00:25:29 +0300

    tried to demonstrate the results

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -3,7 +3,9 @@ Starter for demonstration of laboratory work.
 """
 # pylint: disable= too-many-locals, undefined-variable, unused-import
 from pathlib import Path
-from main import RawDataImporter
+from lab_7_llm.main import RawDataImporter, RawDataPreprocessor
+from core_utils.llm.time_decorator import report_time
+import json
 
 @report_time
 def main() -> None:
@@ -11,7 +13,24 @@ def main() -> None:
     Run the translation pipeline.
     """
     result = None
+
+    with open(Path(__file__).parent / "settings.json", encoding="utf-8") as f:
+        settings_dict = json.load(f)
+
+
+    importer = RawDataImporter(settings_dict["parameters"]["dataset"])
+    importer.obtain()
+
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+    analysis = preprocessor.analyze()
+
+    result = analysis
+    print(result)
+
+    for i in result.values():
+        print(type(i))
     assert result is not None, "Demo does not work correctly"
+
 
 
 if __name__ == "__main__":
