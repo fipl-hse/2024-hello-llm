7092c8a43e2976bca7509495926c4aa4f90679da
Yulia Lopatko <lopatko.julia3@gmail.com>
2025-02-03 18:49:47 +0300

    stiiiiill trying

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import json
 
 from config.constants import PROJECT_ROOT
+from config.lab_settings import LabSettings
 from core_utils.llm.time_decorator import report_time
 from lab_7_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
