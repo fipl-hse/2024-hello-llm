ab983c1f436484613ea68b1137669acf08db47a9
GorbachevaSasha <alexandragorbachewa@yandex.ru>
2025-02-04 02:57:54 +0300

    done start.py and change main.py

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -12,7 +12,7 @@ from typing import Iterable, Sequence
 import pandas as pd
 import torch
 from datasets import load_dataset
-from evaluate import load
+#from evaluate import load
 from pandas import DataFrame
 
 from torch.utils.data import Dataset
--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -3,14 +3,25 @@ Starter for demonstration of laboratory work.
 """
 # pylint: disable= too-many-locals, undefined-variable, unused-import
 from pathlib import Path
+from config.constants import PROJECT_ROOT
+from config.lab_settings import LabSettings
+from core_utils.llm.time_decorator import report_time
+from lab_7_llm.main import RawDataImporter, RawDataPreprocessor
 
+SETTINGS_PATH = PROJECT_ROOT / 'lab_7_llm/settings.json'
 
 @report_time
 def main() -> None:
     """
     Run the translation pipeline.
     """
-    result = None
+    settings = LabSettings(SETTINGS_PATH)
+
+    importer = RawDataImporter(settings.parameters.dataset)
+    importer.obtain()
+
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+    result = preprocessor.analyze()
     assert result is not None, "Demo does not work correctly"
 
 
