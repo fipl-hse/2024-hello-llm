1901d16d049437039f7035dc80c2354ec0b9a192
Sophie <sonyakrasnova42@gmail.com>
2025-02-04 19:17:12 +0300

    work por favor

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -23,7 +23,7 @@ def main() -> None:
     #preprocessor.analyze()
     preprocessor.transform()
     dataset = TaskDataset(preprocessor.data.head(100))
-    pipeline = LLMPipeline(config_file['parameters']['model'], dataset, max_length=120, batch_size=1, device='cpu')
+    pipeline = LLMPipeline(config_file['parameters']['model'], dataset, max_length=120, batch_size=64, device='cpu')
     #print(pipeline.analyze_model())
     infer_sample_result = pipeline.infer_sample(dataset[1])
     assert infer_sample_result is not None, "Demo does not work correctly"
