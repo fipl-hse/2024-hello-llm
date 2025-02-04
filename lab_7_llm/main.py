54d9d5a8eb8328b388df97dab78764b1356c787f
Sophie <sonyakrasnova42@gmail.com>
2025-02-04 17:47:08 +0300

    i hope this works

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -3,25 +3,24 @@ Laboratory work.
 
 Working with Large Language Models.
 """
-from pathlib import Path
-from typing import Iterable, Sequence
-
 import torch
 import pandas as pd
+
 from datasets import load_dataset
-from torchinfo import summary
-from transformers import MarianMTModel, MarianTokenizer
-from pandas import DataFrame
 
-from torch.utils.data import Dataset
-
-from core_utils.llm.time_decorator import report_time
+from core_utils.llm.llm_pipeline import AbstractLLMPipeline
+from core_utils.llm.metrics import Metrics
 from core_utils.llm.raw_data_importer import AbstractRawDataImporter
 from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor, ColumnNames
-from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.task_evaluator import AbstractTaskEvaluator
-from core_utils.llm.metrics import Metrics
+from core_utils.llm.time_decorator import report_time
 
+from pandas import DataFrame
+from pathlib import Path
+from typing import Iterable, Sequence
+from torch.utils.data import Dataset
+from torchinfo import summary
+from transformers import MarianMTModel, MarianTokenizer
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 
 
