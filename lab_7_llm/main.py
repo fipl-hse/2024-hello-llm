9f72b69c1d02f7f701b89167ba44b7433c833c5f
Nastyaperson123 <kalulinaa@gmail.com>
2025-02-05 19:27:13 +0300

    import check delulu?

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -5,16 +5,16 @@ Working with Large Language Models.
 """
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 from pathlib import Path
-import pandas as pd
 from typing import Iterable, Sequence
 
+import pandas as pd
 import torch
 from datasets import load_dataset
 from pandas import DataFrame
-
 from torch.utils.data import Dataset
 from torchinfo import summary
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
 from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.metrics import Metrics
 from core_utils.llm.raw_data_importer import AbstractRawDataImporter
