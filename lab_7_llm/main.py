52d169bd1f5da80d570b078fcada387f5764e179
Sergo-M <mr.sermoz@gmail.com>
2025-02-08 15:39:41 +0300

    infer logic fix

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -217,7 +217,7 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             list[str]: Model predictions as strings
         """
-        inputs = self._tokenizer(list(sample_batch[0]),
+        inputs = self._tokenizer(list(list(zip(*sample_batch))[0]),
                                  return_tensors="pt",
                                  padding=True,
                                  truncation=True).to(self._device)
