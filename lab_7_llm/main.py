b0fafe7833a2ca6ffe4a76c4a797d333a091c0d3
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2025-02-09 00:21:01 +0300

    same promlem

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -190,7 +190,8 @@ class LLMPipeline(AbstractLLMPipeline):
             pd.DataFrame: Data with predictions
         """
         data_loader = DataLoader(batch_size=self._batch_size,
-                                 dataset=self._dataset)
+                                 dataset=self._dataset,
+                                 collate_fn=lambda x: x)
 
         predictions = []
         for batch in data_loader:
