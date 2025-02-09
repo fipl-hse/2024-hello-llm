b8287d4df37b1e51db8d458a8c73c2edad69a355
KRISTINA <kristinachekmareva@yandex.ru>
2025-02-09 15:43:45 +0300

    some changes for 6

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -4,8 +4,10 @@ Starter for demonstration of laboratory work.
 # pylint: disable= too-many-locals, undefined-variable, unused-import
 from pathlib import Path
 import json
+
 from config.constants import PROJECT_ROOT
 from core_utils.llm.time_decorator import report_time
+
 from lab_7_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
