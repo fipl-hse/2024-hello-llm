9dc8e63250f92aef08f739da282c1c4c2ff626d4
sashullel <klen.sasha_22@list.ru>
2025-02-09 15:51:16 +0300

    start service

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -4,6 +4,9 @@ Web service for model inference.
 # pylint: disable=too-few-public-methods, undefined-variable, unused-import, assignment-from-no-return, duplicate-code
 from pathlib import Path
 
+from fastapi import FastAPI
+from lab_7_llm.main import LLMPipeline
+
 
 def init_application() -> tuple[FastAPI, LLMPipeline]:
     """
@@ -15,5 +18,14 @@ def init_application() -> tuple[FastAPI, LLMPipeline]:
         tuple[fastapi.FastAPI, LLMPipeline]: instance of server and pipeline
     """
 
+    app = FastAPI()
+
+    @app.get("/")
+    async def read_root():
+        return {"Hello": "World"}
+
+    @app.get("/items/{item_id}")
+    async def read_item(item_id: int, q: str | None = None):
+        return {"item_id": item_id, "q": q}
 
 app, pipeline = (None, None)
