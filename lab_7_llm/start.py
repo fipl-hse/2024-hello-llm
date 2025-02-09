ab86b8c9e45ef9143c431d70ca922236289dc44c
PolinaDomozhirova <pvdomozhirova@edu.hse.ru>
2025-02-09 13:59:57 +0300

    For a grade of 6

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -2,7 +2,6 @@
 Starter for demonstration of laboratory work.
 """
 # pylint: disable= too-many-locals, undefined-variable, unused-import
-
 from config.constants import PROJECT_ROOT
 from config.lab_settings import LabSettings
 from lab_7_llm.main import (
