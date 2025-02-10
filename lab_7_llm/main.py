446618855348e78ba95aa4b508fbc56331bbba1b
Christina Nigamedzyanova <kristinanigamedzanova@gmail.com>
2025-02-10 19:09:19 +0300

    implemented step 3

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -105,6 +105,7 @@ class TaskDataset(Dataset):
         Args:
             data (pandas.DataFrame): Original data
         """
+        self._data = data
 
     def __len__(self) -> int:
         """
@@ -113,6 +114,7 @@ class TaskDataset(Dataset):
         Returns:
             int: The number of items in the dataset
         """
+        return len(self._data)
 
     def __getitem__(self, index: int) -> tuple[str, ...]:
         """
@@ -124,6 +126,7 @@ class TaskDataset(Dataset):
         Returns:
             tuple[str, ...]: The item to be received
         """
+        return tuple(self._data[ColumnNames.SOURCE.value][index])
 
     @property
     def data(self) -> DataFrame:
@@ -133,6 +136,8 @@ class TaskDataset(Dataset):
         Returns:
             pandas.DataFrame: Preprocessed DataFrame
         """
+        return self._data
+
 
 
 class LLMPipeline(AbstractLLMPipeline):
