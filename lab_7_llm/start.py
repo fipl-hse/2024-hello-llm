37d870095514093099d396fc91e4f5f27005061e
Yulia Lopatko <lopatko.julia3@gmail.com>
2025-02-11 19:54:53 +0300

    directed by robert b weide

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -8,8 +8,14 @@ from pathlib import Path
 
 from config.constants import PROJECT_ROOT
 from config.lab_settings import LabSettings
-from core_utils.llm.time_decorator import report_time
-from lab_7_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset, TaskEvaluator
+from lab_7_llm.main import (
+    LLMPipeline,
+    RawDataImporter,
+    RawDataPreprocessor,
+    report_time,
+    TaskDataset,
+    TaskEvaluator,
+)
 
 
 @report_time
