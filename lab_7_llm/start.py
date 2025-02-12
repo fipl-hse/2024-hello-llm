677068bbba393e3076dbc8ba461d2ee08d24b542
KRISTINA <kristinachekmareva@yandex.ru>
2025-02-12 23:42:24 +0300

    fixing start.py

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -25,21 +25,32 @@ def main() -> None:
 
     importer = RawDataImporter(settings['parameters']['dataset'])
     importer.obtain()
+
+    if importer.raw_data is None:
+        raise ValueError("Raw data is None")
+
     preprocessor = RawDataPreprocessor(importer.raw_data)
+
     analysis = preprocessor.analyze()
     print(analysis)
+
     preprocessor.transform()
     dataset = TaskDataset(preprocessor.data.head(100))
     print(len(dataset))
     print(dataset[1])
+
     pipeline = LLMPipeline(settings['parameters']['model'], dataset, max_length=120, batch_size=1, device='cpu')
     print(pipeline.analyze_model())
+
     infer_sample = pipeline.infer_sample(dataset[1])
     print(infer_sample)
+
     infer_df = pipeline.infer_dataset()
+
     path_to_outputs = PROJECT_ROOT / 'lab_7_llm' / 'dist' / 'predictions.csv'
     path_to_outputs.parent.mkdir(exist_ok=True)
     infer_df.to_csv(path_to_outputs, index=False)
+
     evaluation = TaskEvaluator(path_to_outputs, settings['parameters']['metrics'])
     eval_res = evaluation.run()
     print(eval_res)
