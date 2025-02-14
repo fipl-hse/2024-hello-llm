222be2e46b717c88528b55b99bd22da2de0b08ed
sashullel <klen.sasha_22@list.ru>
2025-02-14 11:13:54 +0300

    codestyle fixes

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -25,7 +25,7 @@ def main() -> None:
 
     importer = RawDataImporter(parameters.dataset)
     importer.obtain()
-    if not importer.raw_data:
+    if importer.raw_data is None:
         return
 
     preprocessor = RawDataPreprocessor(importer.raw_data)
