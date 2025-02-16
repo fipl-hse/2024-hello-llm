f242d7c59586ebf2e2cddd0cebb701f38d8b0492
Luba <ljuba.gorbunova@gmail.com>
2025-02-16 23:33:02 +0300

    what imports am i missing....

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -4,11 +4,12 @@ Starter for demonstration of laboratory work.
 # pylint: disable= too-many-locals, undefined-variable, unused-import
 import json
 from pathlib import Path
+
 import pandas as pd
 
 from config.constants import PROJECT_ROOT
-from core_utils.llm.metrics import Metrics
 from core_utils.llm.time_decorator import report_time
+from core_utils.llm.metrics import Metrics
 from lab_7_llm.main import (LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset,
                             TaskEvaluator)
 
