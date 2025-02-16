4d9c4eebbaeb7aafb8d65683c5e13a0c8e7aeda1
Luba <ljuba.gorbunova@gmail.com>
2025-02-16 22:52:50 +0300

    start.py import style fix

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -8,10 +8,7 @@ from pathlib import Path
 from config.constants import PROJECT_ROOT
 from core_utils.llm.metrics import Metrics
 from core_utils.llm.time_decorator import report_time
-from lab_7_llm.main import (LLMPipeline,
-                            RawDataImporter,
-                            RawDataPreprocessor,
-                            TaskDataset,
+from lab_7_llm.main import (LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset,
                             TaskEvaluator)
 
 
