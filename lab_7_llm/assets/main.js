6829ce9cf2026df5dd153e0d4fc086a71bbdddd3
vchemsmisl <issakov.daanila@mail.ru>
2025-02-17 16:41:02 +0300

    working on a web-server [mark 10]

--- lab_7_llm/assets/main.js
+++ lab_7_llm/assets/main.js
@@ -5,19 +5,19 @@ button.addEventListener('click', async () => {
     const textInput = document.getElementById('textInput').value;
     const resultDiv = document.getElementById('result');
 
-let response = await fetch('/infer', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ question: textInput })
-});
+    let response = await fetch('/infer', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ question: textInput })
+    });
 
-if (response.ok) {
-    let result = await response.json();
-} else {
-    alert('Ошибка HTTP: ' + response.status);
-}
+    if (response.ok) {
+        let result = await response.json();
+        resultDiv.textContent = result.infer;
+    } else {
+        alert('Ошибка HTTP: ' + response.status);
+    }
 
-resultDiv.textContent = result.infer;
 });
