2578604a0b0a8ece7d55accd1b7984ea5d1fe77f
vchemsmisl <issakov.daanila@mail.ru>
2025-02-17 16:51:33 +0300

    code style fixes [mark 10]

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -4,6 +4,7 @@ Web service for model inference.
 # pylint: disable=too-few-public-methods, undefined-variable, unused-import, assignment-from-no-return, duplicate-code
 from pathlib import Path
 
+import pandas as pd
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
@@ -11,11 +12,14 @@ from fastapi.templating import Jinja2Templates
 from pydantic.dataclasses import dataclass
 
 from config.lab_settings import LabSettings
-from lab_7_llm.main import LLMPipeline
+from lab_7_llm.main import LLMPipeline, TaskDataset
 
 
 @dataclass
 class Query:
+    """
+    Class for query content.
+    """
     question: str
 
 
@@ -32,6 +36,7 @@ def init_application() -> tuple[FastAPI, LLMPipeline]:
     batch_size = 1
     device = 'cpu'
     settings_path = Path(__file__).parent / 'settings.json'
+    empty_dataset = TaskDataset(pd.DataFrame())
 
     fastapi_app = FastAPI()
     fastapi_app.mount('/static',
@@ -39,7 +44,7 @@ def init_application() -> tuple[FastAPI, LLMPipeline]:
 
     settings = LabSettings(settings_path)
     llm_pipeline = LLMPipeline(settings.parameters.model,
-                           None,
+                           empty_dataset,
                            max_length, batch_size, device)
 
     return fastapi_app, llm_pipeline
@@ -48,12 +53,29 @@ def init_application() -> tuple[FastAPI, LLMPipeline]:
 app, pipeline = init_application()
 
 @app.get('/', response_class=HTMLResponse)
-async def root(request: Request):
+async def root(request: Request) -> Jinja2Templates.TemplateResponse:
+    """
+    Root endpoint.
+
+    Args:
+        request: GET-request to the server
+
+    Returns:
+        HTML-template
+    """
     templates = Jinja2Templates(directory=f'{Path(__file__).parent}/assets')
     return templates.TemplateResponse('index.html', {'request': request})
 
 @app.post('/infer')
-async def infer(query: Query):
+async def infer(query: Query) -> JSONResponse:
+    """
+    Main endpoint for model inference.
+
+    Args:
+        query: Query instance, containing text to classify
+
+    Returns:
+        JSONResponse instance in {"infer": prediction} format
+    """
     prediction = pipeline.infer_sample((query.question, ))
-    print(prediction)
     return JSONResponse(content={'infer': prediction})
