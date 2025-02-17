3e0ed03c3f703aebb9ce7f9aa9c4f0680a35ab1b
milana_hodzhametova <milana.hodzhametova12@gmail.com>
2025-02-17 22:26:42 +0300

    changed

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -4,6 +4,7 @@ Web service for model inference.
 # pylint: disable=too-few-public-methods, undefined-variable, unused-import, assignment-from-no-return, duplicate-code
 from pathlib import Path
 from fastapi import FastAPI
+from lab_7_llm.main import LLMPipeline
 
 
 def init_application() -> tuple[FastAPI, LLMPipeline]:
