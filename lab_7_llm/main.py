7e0b94b64897c93e80094e0d037ac49d2396b50d
Egor <egordzyumenko@yandex.ru>
2025-02-17 19:30:47 +0300

    sorting

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -6,23 +6,21 @@ Working with Large Language Models.
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 from pathlib import Path
 from typing import Iterable, Sequence
-from datasets import load_dataset
-import string
 
+import fastapi
 import pandas as pd
-import numpy as np
+import torch
+from datasets import load_dataset
 from torch.utils.data import Dataset
+from torchinfo import summary
+from transformers import AutoModelForTokenClassification, AutoTokenizer
+
 from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.metrics import Metrics
 from core_utils.llm.raw_data_importer import AbstractRawDataImporter
 from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor
 from core_utils.llm.task_evaluator import AbstractTaskEvaluator
 from core_utils.llm.time_decorator import report_time
-import re
-from torchinfo import summary
-from transformers import AutoModelForTokenClassification, AutoTokenizer
-import torch
-import fastapi
 
 
 class RawDataImporter(AbstractRawDataImporter):
