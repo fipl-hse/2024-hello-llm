033a8a412895f39284db320ed697cd3bf901e7da
Nastyaperson123 <kalulinaa@gmail.com>
2025-02-19 02:18:46 +0300

    import fixing again 2

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 import pandas as pd
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic.dataclasses import dataclass
