ad0f6e0d6fddd872f2b0aeda97a2083676aa38ec
Sophie <sonyakrasnova42@gmail.com>
2025-02-18 16:50:25 +0300

    for mark 10

--- lab_7_llm/service.py
+++ lab_7_llm/service.py
@@ -1,12 +1,20 @@
 """
 Web service for model inference.
 """
-# pylint: disable=too-few-public-methods, undefined-variable, unused-import, assignment-from-no-return, duplicate-code
 from pathlib import Path
 
-from fastapi import FastAPI
+import pandas as pd
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic.dataclasses import dataclass
 
-from lab_7_llm.main import LLMPipeline
+from config.lab_settings import LabSettings
+from lab_7_llm.main import LLMPipeline, TaskDataset
+
+PARENT_DIR = Path(__file__).parent
+ASSETS_PATH = PARENT_DIR / 'assets'
 
 
 def init_application() -> tuple[FastAPI, LLMPipeline]:
@@ -18,6 +26,48 @@ def init_application() -> tuple[FastAPI, LLMPipeline]:
     Returns:
         tuple[fastapi.FastAPI, LLMPipeline]: instance of server and pipeline
     """
+    settings = LabSettings(PARENT_DIR / 'settings.json')
+
+    model_app = FastAPI()
+    model_app.mount('/assets', StaticFiles(directory=ASSETS_PATH), name='assets')
+
+    model_pipeline = LLMPipeline(model_name=settings.parameters.model,
+                                 dataset=TaskDataset(pd.DataFrame()), max_length=120, batch_size=1,
+                                 device='cpu')
+    return model_app, model_pipeline
 
 
-app, pipeline = (None, None)
+app, pipeline = init_application()
+templates = Jinja2Templates(directory=ASSETS_PATH)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request) -> HTMLResponse:
+    """
+    Creates a root endpoint of the service.
+    Args:
+        request (Request): passed data for HTML.
+    Returns:
+        HTMLResponse: an HTML page
+    """
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@dataclass
+class Query:
+    """
+    A class that contains text of the query.
+    """
+    question: str
+
+
+@app.post("/infer")
+def infer(query: Query):
+    """
+    Creates a main endpoint for model call.
+    Args:
+        query (Query): Passed data for HTML.
+    Returns:
+        dictionary: Response obtained as a result of the pipeline.
+    """
+    return {"infer": pipeline.infer_sample((query.question,))}
