f9eb226c3f19c3bff270acbdd418d8f3818884ae
yourMasya <mmatvs@mail.ru>
2025-03-05 13:36:27 +0300

    blya

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -5,13 +5,11 @@ Working with Large Language Models.
 """
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 import re
-
 from pathlib import Path
 from typing import Iterable, Sequence
 
 import pandas as pd
 import torch
-
 from datasets import load_dataset
 from evaluate import load
 from pandas import DataFrame
