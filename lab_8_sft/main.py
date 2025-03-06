d00d6492b1f53605635f9b73fb7219a608376bb2
cybern00dle <koshech_noskich@mail.ru>
2025-03-07 01:38:11 +0300

    trying for 6

--- lab_8_sft/main.py
+++ lab_8_sft/main.py
@@ -7,6 +7,23 @@ Fine-tuning Large Language Models for a downstream task.
 from pathlib import Path
 from typing import Iterable, Sequence
 
+import evaluate
+import pandas as pd
+import torch
+from datasets import load_dataset
+from torch.utils.data import DataLoader, Dataset
+from torchinfo import summary
+from transformers import AutoTokenizer, BertTokenizerFast, EncoderDecoderModel
+
+from config.lab_settings import SFTParams
+from core_utils.llm.llm_pipeline import AbstractLLMPipeline
+from core_utils.llm.metrics import Metrics
+from core_utils.llm.raw_data_importer import AbstractRawDataImporter
+from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor, ColumnNames
+from core_utils.llm.sft_pipeline import AbstractSFTPipeline
+from core_utils.llm.task_evaluator import AbstractTaskEvaluator
+from core_utils.llm.time_decorator import report_time
+
 
 class RawDataImporter(AbstractRawDataImporter):
     """
@@ -18,7 +35,13 @@ class RawDataImporter(AbstractRawDataImporter):
         """
         Import dataset.
         """
-        pass
+        self._raw_data = load_dataset(
+            self._hf_name,
+            split='test'
+        ).to_pandas()
+
+        if not isinstance(self._raw_data, pd.DataFrame):
+            raise TypeError('The downloaded dataset is not pd.DataFrame.')
 
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
@@ -33,12 +56,27 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         Returns:
             dict: dataset key properties.
         """
+        properties = {
+            'dataset_number_of_samples': self._raw_data.shape[0],
+            'dataset_columns': self._raw_data.shape[1],
+            'dataset_duplicates': self._raw_data.duplicated().sum(),
+            'dataset_empty_rows': self._raw_data.isnull().any(axis=1).sum(),
+            'dataset_sample_min_len': self._raw_data['article'].dropna().str.len().min(),
+            'dataset_sample_max_len': self._raw_data['article'].dropna().str.len().max()
+        }
+        return properties
 
     @report_time
     def transform(self) -> None:
         """
         Apply preprocessing transformations to the raw dataset.
         """
+        self._data = self._raw_data.copy()
+        self._data.rename(columns={
+            'abstract': ColumnNames.TARGET.value,
+            'article': ColumnNames.SOURCE.value
+        }, inplace=True)
+        self._data.reset_index(inplace=True)
 
 
 class TaskDataset(Dataset):
@@ -53,6 +91,7 @@ class TaskDataset(Dataset):
         Args:
             data (pandas.DataFrame): Original data
         """
+        self._data = data
 
     def __len__(self) -> int:
         """
@@ -61,6 +100,7 @@ class TaskDataset(Dataset):
         Returns:
             int: The number of items in the dataset
         """
+        return self._data.shape[0]
 
     def __getitem__(self, index: int) -> tuple[str, ...]:
         """
@@ -72,15 +112,17 @@ class TaskDataset(Dataset):
         Returns:
             tuple[str, ...]: The item to be received
         """
+        return (str(self._data.loc[index, ColumnNames.SOURCE.value]),)
 
     @property
-    def data(self) -> DataFrame:
+    def data(self) -> pd.DataFrame:
         """
         Property with access to preprocessed DataFrame.
 
         Returns:
             pandas.DataFrame: Preprocessed DataFrame
         """
+        return self._data
 
 
 def tokenize_sample(
@@ -154,6 +196,9 @@ class LLMPipeline(AbstractLLMPipeline):
             batch_size (int): The size of the batch inside DataLoader.
             device (str): The device for inference.
         """
+        super().__init__(model_name, dataset, max_length, batch_size, device)
+        self._model = EncoderDecoderModel.from_pretrained(model_name).to(device)
+        self._tokenizer = BertTokenizerFast.from_pretrained(model_name)
 
     def analyze_model(self) -> dict:
         """
@@ -162,6 +207,27 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             dict: Properties of a model
         """
+        if not isinstance(self._model, torch.nn.Module):
+            raise TypeError('The model is not a torch.nn.Module instance.')
+
+        tensor = torch.zeros(1, self._model.config.decoder.max_position_embeddings, dtype=torch.long)
+        model_summary = summary(
+            self._model,
+            input_data={'input_ids': tensor, 'attention_mask': tensor, 'decoder_input_ids': tensor},
+            device='cpu'
+        )
+
+        properties = {
+            'input_shape': list(tensor.size()),
+            'embedding_size': self._model.config.decoder.max_position_embeddings,
+            'output_shape': model_summary.summary_list[-1].output_size,
+            'num_trainable_params': model_summary.trainable_params,
+            'vocab_size': self._model.config.vocab_size,
+            'size': model_summary.total_param_bytes,
+            'max_context_length': self._model.config.max_length
+        }
+
+        return properties
 
     @report_time
     def infer_sample(self, sample: tuple[str, ...]) -> str | None:
@@ -174,6 +240,7 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             str | None: A prediction
         """
+        return self._infer_batch([sample])[0]
 
     @report_time
     def infer_dataset(self) -> pd.DataFrame:
@@ -183,6 +250,13 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             pd.DataFrame: Data with predictions
         """
+        dataloader = DataLoader(dataset=self._dataset, batch_size=self._batch_size)
+        dataframe = pd.DataFrame(self._dataset.data)
+        predicted = []
+        for batch in dataloader:
+            predicted.extend(self._infer_batch(batch))
+        dataframe[ColumnNames.PREDICTION.value] = predicted
+        return dataframe
 
     @torch.no_grad()
     def _infer_batch(self, sample_batch: Sequence[tuple[str, ...]]) -> list[str]:
@@ -195,6 +269,25 @@ class LLMPipeline(AbstractLLMPipeline):
         Returns:
             list[str]: model predictions as strings
         """
+        if not isinstance(self._model, torch.nn.Module):
+            raise TypeError('The model is not a torch.nn.Module instance.')
+
+        inputs = self._tokenizer(
+            [j for i in sample_batch for j in i],
+            max_length=self._max_length,
+            padding=True,
+            truncation=True,
+            return_tensors='pt'
+        )
+        input_ids = inputs.input_ids.to(self._device)
+        attention_mask = inputs.attention_mask.to(self._device)
+        outputs = self._model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            max_length=self._max_length
+        )
+        predicted = self._tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        return [str(i) for i in predicted]
 
 
 class TaskEvaluator(AbstractTaskEvaluator):
@@ -210,6 +303,8 @@ class TaskEvaluator(AbstractTaskEvaluator):
             data_path (pathlib.Path): Path to predictions
             metrics (Iterable[Metrics]): List of metrics to check
         """
+        super().__init__(metrics)
+        self._data_path = data_path
 
     def run(self) -> dict | None:
         """
@@ -218,6 +313,19 @@ class TaskEvaluator(AbstractTaskEvaluator):
         Returns:
             dict | None: A dictionary containing information about the calculated metric
         """
+        dataframe = pd.read_csv(self._data_path)
+        metrics_scores = {}
+        for metric in self._metrics:
+            if metric.value == 'rouge':
+                evaluator = evaluate.load(metric.value, seed=77)
+            else:
+                evaluator = evaluate.load(metric.value)
+            score = evaluator.compute(
+                references=dataframe[ColumnNames.TARGET.value],
+                predictions=dataframe[ColumnNames.PREDICTION.value]
+            )
+            metrics_scores[metric.value] = score['rougeL' if metric.value == 'rouge' else metric.value]
+        return metrics_scores
 
 
 class SFTPipeline(AbstractSFTPipeline):
