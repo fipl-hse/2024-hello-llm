f8f5cecf4bc2177120e59f74f37e379c292e1d93
milana_hodzhametova <milana.hodzhametova12@gmail.com>
2025-03-07 01:30:18 +0300

    changed

--- lab_8_sft/start.py
+++ lab_8_sft/start.py
@@ -2,13 +2,15 @@
 Fine-tuning starter.
 """
 # pylint: disable=too-many-locals, undefined-variable, unused-import, too-many-branches, too-many-statements
+from pathlib import Path
+
 from config.constants import PROJECT_ROOT
 from config.lab_settings import LabSettings
-from lab_7_llm.main import (
+from core_utils.llm.time_decorator import report_time
+from lab_8_sft.main import (
     LLMPipeline,
     RawDataImporter,
     RawDataPreprocessor,
-    report_time,
     TaskDataset,
     TaskEvaluator,
 )
