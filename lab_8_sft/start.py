2522fe79da6f60d1485d187e8f7360dedbcd3a4d
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2025-03-06 12:26:36 +0300

    I forgot about start.py

--- lab_8_sft/start.py
+++ lab_8_sft/start.py
@@ -4,13 +4,54 @@ Fine-tuning starter.
 # pylint: disable=too-many-locals, undefined-variable, unused-import, too-many-branches, too-many-statements
 from pathlib import Path
 
+from config.constants import PROJECT_ROOT
+from config.lab_settings import LabSettings
+from core_utils.llm.time_decorator import report_time
+from lab_8_sft.main import (
+    LLMPipeline,
+    RawDataImporter,
+    RawDataPreprocessor,
+    SFTPipeline,
+    TaskDataset,
+    TaskEvaluator,
+    TokenizedTaskDataset,
+)
+
 
 @report_time
 def main() -> None:
     """
     Run the translation pipeline.
     """
-    result = None
+    lab_settings = LabSettings(PROJECT_ROOT / 'lab_8_sft' / 'settings.json')
+    importer = RawDataImporter(lab_settings.parameters.dataset)
+    importer.obtain()
+
+    if importer.raw_data is None:
+        return
+
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+    properties = preprocessor.analyze()
+    print(properties)
+
+    preprocessor.transform()
+    dataset = TaskDataset(preprocessor.data.head(100))
+
+    pipeline = LLMPipeline(lab_settings.parameters.model,
+                           dataset,
+                           max_length=120,
+                           batch_size=64,
+                           device="cpu")
+
+    pipeline.analyze_model()
+
+    dataframe = pipeline.infer_dataset()
+
+    predictions_path = PROJECT_ROOT / "lab_8_sft" / "dist" / "predictions.csv"
+    predictions_path.parent.mkdir(exist_ok=True)
+    dataframe.to_csv(predictions_path)
+
+    result = dataframe
     assert result is not None, "Finetuning does not work correctly"
 
 
