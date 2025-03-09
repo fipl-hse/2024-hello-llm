e137918a62fc57ca69ddc8af2e49a671d4b3f295
Sophie <sonyakrasnova42@gmail.com>
2025-03-09 11:26:06 +0300

    trying for 10

---
+++ lab_8_sft/assets/main.js
@@ -0,0 +1,39 @@
+const checkbox = document.getElementById('base-model-checkbox');
+const clickButton = document.getElementById("send-button");
+const handleClick = async () => {
+    try {
+        const premise = document.getElementById("premise").value;
+        const hypothesis = document.getElementById("hypothesis").value;
+        const isChecked = checkbox.checked;
+        const result = document.getElementById("prediction");
+
+        if (!premise || !hypothesis) {
+            result.textContent = "You need to type two texts!";
+            return;
+        }
+
+        const response = await fetch("/infer", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                question: premise,
+                hypothesis: hypothesis,
+                is_base_model: isChecked})
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        result.innerText = data.infer;
+
+    } catch (error) {
+        console.error('Error during inference:', error);
+        result.textContent = `Error: ${error.message}`;
+    }
+};
+
+clickButton.addEventListener('click', handleClick);
--- lab_8_sft/service.py
+++ lab_8_sft/service.py
@@ -1,10 +1,20 @@
 """
 Web service for model inference.
 """
-# pylint: disable=too-few-public-methods, undefined-variable, unused-import, assignment-from-no-return, duplicate-code
-from fastapi import FastAPI
+from pathlib import Path
 
-from lab_8_sft.main import LLMPipeline
+import pandas as pd
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic.dataclasses import dataclass
+
+from config.lab_settings import LabSettings
+from lab_8_sft.main import LLMPipeline, TaskDataset
+
+PARENT_DIR = Path(__file__).parent
+ASSETS_PATH = PARENT_DIR / 'assets'
 
 
 def init_application() -> tuple[FastAPI, LLMPipeline, LLMPipeline]:
@@ -16,6 +26,61 @@ def init_application() -> tuple[FastAPI, LLMPipeline, LLMPipeline]:
     Returns:
         tuple[fastapi.FastAPI, LLMPipeline, LLMPipeline]: instance of server and pipeline
     """
+    settings = LabSettings(PARENT_DIR / 'settings.json')
+
+    model_app = FastAPI()
+    model_app.mount('/assets', StaticFiles(directory=ASSETS_PATH), name='assets')
+
+    pre_trained_llm = LLMPipeline(model_name=settings.parameters.model, max_length=120,
+                                  dataset=TaskDataset(pd.DataFrame()), batch_size=1, device='cpu')
+
+    fine_tuned_model_path = PARENT_DIR / 'dist' / settings.parameters.model
+
+    fine_tuned_llm = LLMPipeline(model_name=str(fine_tuned_model_path), max_length=120,
+                                 dataset=TaskDataset(pd.DataFrame()), batch_size=1, device='cpu')
+
+    return model_app, pre_trained_llm, fine_tuned_llm
 
 
-app, pre_trained_pipeline, fine_tuned_pipeline = (None, None, None)
+app, pre_trained_pipeline, fine_tuned_pipeline = init_application()
+templates = Jinja2Templates(directory=ASSETS_PATH)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request) -> HTMLResponse:
+    """
+    Creates a root endpoint of the service.
+    Args:
+        request (Request): passed data for HTML.
+    Returns:
+        HTMLResponse: an HTML page
+    """
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@dataclass
+class Query:
+    """
+    A class that contains text of the query.
+    """
+    question: str
+    hypothesis: str
+    is_base_model: bool
+
+
+@app.post("/infer")
+def infer(query: Query) -> dict[str, str]:
+    """
+    Creates a main endpoint for model call.
+    Args:
+        query (Query): Passed data for HTML.
+    Returns:
+        dict[str, str]: Inference results as a dictionary.
+    """
+    label_mapping = {'0': 'entailment', '1': 'neutral', '2': 'contradiction'}
+    sample = query.question, query.hypothesis
+    if query.is_base_model:
+        prediction = pre_trained_pipeline.infer_sample(sample)
+        return {'infer': label_mapping.get(prediction)}
+    prediction = fine_tuned_pipeline.infer_sample(sample)
+    return {'infer': label_mapping.get(prediction)}
