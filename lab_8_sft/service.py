82155bf6fd3f2ff6f99323d9cbf8abc68a73ab0a
Sophie <sonyakrasnova42@gmail.com>
2025-03-09 13:43:12 +0300

    if only

--- lab_8_sft/service.py
+++ lab_8_sft/service.py
@@ -85,7 +85,7 @@ def infer(query: Query) -> dict[str, str]:
     if not query.hypothesis or not query.question:
         return {'infer': 'ERROR! Fill both text fields!'}
     if query.is_base_model:
-        prediction = str(pre_trained_pipeline.infer_sample(sample))
+        prediction = pre_trained_pipeline.infer_sample(sample)
     else:
-        prediction = str(fine_tuned_pipeline.infer_sample(sample))
-    return {'infer': label_mapping.get(prediction)}
+        prediction = fine_tuned_pipeline.infer_sample(sample)
+    return {'infer': label_mapping.get(prediction, 'unknown')}
