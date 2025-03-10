ced69bde8f2bd9e05d6693e2012cc5e1e068bb25
Tasenko03 <93547677+Tasenko03@users.noreply.github.com>
2025-03-10 21:24:56 +0300

    mark 8

--- lab_8_sft/start.py
+++ lab_8_sft/start.py
@@ -2,15 +2,19 @@
 Fine-tuning starter.
 """
 # pylint: disable=too-many-locals, undefined-variable, unused-import, too-many-branches, too-many-statements
+from transformers import AutoTokenizer
+
 from config.constants import PROJECT_ROOT
-from config.lab_settings import LabSettings
+from config.lab_settings import LabSettings, SFTParams
+from core_utils.llm.time_decorator import report_time
 from lab_8_sft.main import (
     LLMPipeline,
     RawDataImporter,
     RawDataPreprocessor,
-    report_time,
+    SFTPipeline,
     TaskDataset,
     TaskEvaluator,
+    TokenizedTaskDataset,
 )
 
 
@@ -19,6 +23,7 @@ def main() -> None:
     """
     Run the translation pipeline.
     """
+    # for mark 6
     lab_settings = LabSettings(PROJECT_ROOT / 'lab_8_sft' / 'settings.json')
     importer = RawDataImporter(lab_settings.parameters.dataset)
     importer.obtain()
@@ -27,8 +32,7 @@ def main() -> None:
         return
 
     preprocessor = RawDataPreprocessor(importer.raw_data)
-    properties = preprocessor.analyze()
-    print(properties)
+    preprocessor.analyze()
 
     preprocessor.transform()
     dataset = TaskDataset(preprocessor.data.head(100))
@@ -49,7 +53,85 @@ def main() -> None:
 
     evaluator = TaskEvaluator(predictions_path, lab_settings.parameters.metrics)
     result = evaluator.run()
-    print("Evaluation results:")
+    print("Evaluation results (mark 6):")
+    for metric, value in result.items():
+        print(metric, value, sep=': ')
+
+    # for mark 8
+    # Inference
+    preprocessor.transform()
+    num_samples = 10
+    dataset = TaskDataset(preprocessor.data.head(100))
+    pipeline = LLMPipeline(model_name=lab_settings.parameters.model,
+                           dataset=dataset,
+                           max_length=120,
+                           batch_size=64,
+                           device="cpu")
+
+    model_analysis = pipeline.analyze_model()
+    print("Model analysis (inference):")
+    for field, value in model_analysis.items():
+        print(field, value, sep=': ')
+
+    pipeline.infer_dataset()
+
+    predictions_dataframe = pipeline.infer_dataset()
+    predictions_path = PROJECT_ROOT / "lab_8_sft" / "dist" / "predictions.csv"
+    predictions_path.parent.mkdir(exist_ok=True)
+    predictions_dataframe.to_csv(predictions_path)
+
+    evaluator = TaskEvaluator(predictions_path, lab_settings.parameters.metrics)
+    result = evaluator.run()
+    print("Evaluation metrics (inference):")
+    for metric, value in result.items():
+        print(metric, value, sep=': ')
+
+    # Fine-tuning
+    sft_params = SFTParams(
+        max_length=120,
+        batch_size=3,
+        max_fine_tuning_steps=50,
+        device="cpu",
+        finetuned_model_path=PROJECT_ROOT / "lab_8_sft" / "dist" / lab_settings.parameters.model,
+        learning_rate=1e-3
+    )
+
+    fine_tune_samples = sft_params.batch_size * sft_params.max_fine_tuning_steps
+    tokenized_dataset = TokenizedTaskDataset(
+        preprocessor.data.loc[num_samples: num_samples + fine_tune_samples],
+        tokenizer=AutoTokenizer.from_pretrained(lab_settings.parameters.model),
+        max_length=sft_params.max_length
+    )
+
+    sft_pipeline = SFTPipeline(model_name=lab_settings.parameters.model,
+                               dataset=tokenized_dataset,
+                               sft_params=sft_params)
+    sft_pipeline.run()
+
+    num_samples = 10
+    pipeline = LLMPipeline(
+        model_name=str(PROJECT_ROOT / "lab_8_sft" / "dist" / lab_settings.parameters.model),
+        dataset=TaskDataset(preprocessor.data.head(num_samples)),
+        max_length=120,
+        batch_size=64,
+        device="cpu"
+    )
+
+    model_analysis = pipeline.analyze_model()
+    print("Model analysis (fine-tuning):")
+    for field, value in model_analysis.items():
+        print(field, value, sep=': ')
+
+    pipeline.infer_dataset()
+
+    predictions_dataframe = pipeline.infer_dataset()
+    predictions_path = PROJECT_ROOT / "lab_8_sft" / "dist" / "predictions.csv"
+    predictions_path.parent.mkdir(exist_ok=True)
+    predictions_dataframe.to_csv(predictions_path)
+
+    evaluator = TaskEvaluator(predictions_path, lab_settings.parameters.metrics)
+    result = evaluator.run()
+    print("Evaluation metrics (fine-tuning):")
     for metric, value in result.items():
         print(metric, value, sep=': ')
 
