8ddaa4fc7423fb9fcac8d877fd4b8a4f0c1d1b80
solonar <zakker090@gmail.com>
2025-03-11 12:01:46 +0300

    ...ок

--- lab_8_sft/start.py
+++ lab_8_sft/start.py
@@ -131,7 +131,7 @@ def main() -> None:
     sft_params = SFTParams(
         batch_size=3,
         max_length=120,
-        max_fine_tuning_steps=75,
+        max_fine_tuning_steps=100,
         learning_rate=1e-3,
         finetuned_model_path=PROJECT_ROOT / "lab_8_sft" / "dist" / settings.parameters.model,
         device="cpu"
@@ -155,7 +155,7 @@ def main() -> None:
     #                                                         "lab_8_sft" /
     #                                                         "dist" / settings.parameters.model))
 
-    dataset = TaskDataset(preprocessor.data.head(100))
+    dataset = TaskDataset(preprocessor.data.head(10))
     pipeline = LLMPipeline(str(sft_params.finetuned_model_path),
                            dataset, max_length=120,
                            batch_size=64,
