7b09bf9e687f409371b5a9eea12a59c2215dc7e6
Christina Nigamedzyanova <kristinanigamedzanova@gmail.com>
2025-03-12 02:36:20 +0300

    ryjbn;tehnme'tm'

--- lab_8_sft/start.py
+++ lab_8_sft/start.py
@@ -63,9 +63,9 @@ def main() -> None:
 
     finetuned_model_path = Path(__file__).parent / 'dist' / settings.parameters.model
     sft_params = SFTParams(
-        batch_size=2,
+        batch_size=3,
         max_length=120,
-        max_fine_tuning_steps=175,
+        max_fine_tuning_steps=200,
         learning_rate=1e-2,
         device="cpu",
         finetuned_model_path=finetuned_model_path
