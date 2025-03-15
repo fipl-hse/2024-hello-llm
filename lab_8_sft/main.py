9d0f3ade968f2e3154f2352f3d550ba5c7448a61
yourMasya <mmatvs@mail.ru>
2025-03-15 17:54:46 +0300

    demid lezhat

--- lab_8_sft/main.py
+++ lab_8_sft/main.py
@@ -26,7 +26,10 @@ from config.lab_settings import SFTParams
 from core_utils.llm.llm_pipeline import AbstractLLMPipeline
 from core_utils.llm.metrics import Metrics
 from core_utils.llm.raw_data_importer import AbstractRawDataImporter
-from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor, ColumnNames
+from core_utils.llm.raw_data_preprocessor import (
+    AbstractRawDataPreprocessor,
+    ColumnNames
+)
 from core_utils.llm.sft_pipeline import AbstractSFTPipeline
 from core_utils.llm.task_evaluator import AbstractTaskEvaluator
 from core_utils.llm.time_decorator import report_time
