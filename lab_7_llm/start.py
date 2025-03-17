7ae0258adfed420bd95aec00142130d7f228834e
pdshh <pdshamanina@gmail.com>
2025-03-17 13:28:15 +0300

    fix

--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 from config.constants import PROJECT_ROOT
 from config.lab_settings import LabSettings
+from core_utils.llm.time_decorator import report_time
 from lab_7_llm.main import LLMPipeline, RawDataImporter, RawDataPreprocessor, TaskDataset
 
 
