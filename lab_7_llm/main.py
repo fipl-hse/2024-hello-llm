f843ba70a59791705f8e0bb3cd5c62f3f312ddc9
Maydaymywaytoheaven <tobakunina@edu.hse.ru>
2025-03-18 16:34:25 +0300

    this was way more effort than it's worth ngl

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -49,12 +49,12 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         Returns:
             dict: Dataset key properties
         """
-        key_properties = {'dataset_number_of_samples': len(self._raw_data.index),
-                          'dataset_columns': len(self._raw_data.columns),
+        key_properties = {'dataset_number_of_samples': self._raw_data.shape[0],
+                          'dataset_columns': self._raw_data.shape[1],
                           'dataset_duplicates': self._raw_data.duplicated().sum(),
-                          'dataset_empty_rows': self._raw_data.isnull().sum(),
-                          'dataset_sample_min_len': self._raw_data.dropna().min(),
-                          'dataset_sample_max_len': self._raw_data.dropna().max()}
+                          'dataset_empty_rows': self._raw_data.isnull().any(axis=1).sum(),
+                          'dataset_sample_min_len': self._raw_data['comment_text'].dropna().str.len().min(),
+                          'dataset_sample_max_len': self._raw_data['comment_text'].dropna().str.len().max()}
         return key_properties
 
     @report_time
