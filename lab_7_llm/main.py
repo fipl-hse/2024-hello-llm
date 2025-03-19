02c82b3c6ce300cc3c4c3daed46f8dbf21070b4b
pdshh <pdshamanina@gmail.com>
2025-03-19 09:00:00 +0300

    fix

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -38,7 +38,7 @@ class RawDataImporter(AbstractRawDataImporter):
             TypeError: In case of downloaded dataset is not pd.DataFrame
         """
         data = datasets.load_dataset(self._hf_name, split='train')
-        self._raw_data = pd.DataFrame(data)
+        self._raw_data = data.to_pandas()
 
         if not isinstance(self._raw_data, pd.DataFrame):
             raise TypeError('Downloaded dataset is not a pandas DataFrame.')
